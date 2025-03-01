6953ae258320a36e9a59b2eb1d7ee3f48605e2d9
ArifulIslam497431 <arifulislam110406@gmail.com>
2025-03-01 19:25:37 +0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,7 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyBHzvXJJP10P1ofwXgXwg7Sg2xTODXce1g",
   authDomain: "mcq-exam-ea9e2.firebaseapp.com",
@@ -9,10 +11,10 @@ const firebaseConfig = {
   appId: "1:468489593575:web:d3a02e49642baef8e0f842",
   measurementId: "G-Q7WV02X775"
 };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+// ✅ Export Firestore and Auth
+export { db, auth };
